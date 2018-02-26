495d2bc4b01f5ce159ce498ca135d8694e9d8770
Patti Philippe  43197 <G43197@PDG.ESI.HEB.SITE>
2018-02-26 10:16:09 +0100

    commit 4 : le 3 a foiré

--- case.h
+++ case.h
@@ -1,10 +1,28 @@
 #ifndef CASE_H
 #define CASE_H
 
-define namespace
 /*
 10: analyse
 40: model et console
 50: remise graphique 20 et examen 30
 */
+
+enum CaseState{
+    dft,
+    marked,
+    revealed
+};
+
+class Case{
+private:
+    int indice;
+    int line;
+    int column;
+    CaseState state;
+
+public:
+    Case(line, column);
+
+};
+
 #endif // CASE_H
